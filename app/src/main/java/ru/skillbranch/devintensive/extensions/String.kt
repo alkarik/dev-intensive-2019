ca8fbf8874eeea6b454ed347eb79bf244a4bdaa2
alk <alkarik@ya.ru>
2019-07-05 09:46:18 +1000

    hometask_2 update truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,9 +4,9 @@ fun String.truncate(cnt:Int=16):String{
     var str:String =this
     var str1:String
     if(str.length>cnt) {
-        str1=str.substring(0,cnt+1)
-        if(str1.substring(cnt,cnt+1)==" ") str1=str1.trimEnd()
-        if(str.substring(cnt+1).trimMargin().length!=0) str1 = str1.plus("...")
+        str1=str.substring(0,cnt)
+        if(str1.substring(cnt-1,cnt)==" ") str1=str1.trimEnd()
+        if(str.substring(cnt).trimMargin().length!=0) str1 = str1.plus("...")
         return str1
     }else {
         return str.trimEnd()
